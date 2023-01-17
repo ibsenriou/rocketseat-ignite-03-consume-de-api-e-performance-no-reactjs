24c647f73b68cc61eeaafaf9e1f59fc0a4e87089
Gabriel Santos <ibsenriou@hotmail.com>
2023-01-17 19:08:49 -0300

    Project Finished

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,24 +1,27 @@
 import { useContextSelector } from 'use-context-selector'
 import { TransactionsContext } from '../contexts/TransactionsContext'
+import { useMemo } from 'react'
 
 export function useSummary() {
   const transactions = useContextSelector(TransactionsContext, (context) => {
     return context.transactions
   })
 
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.amount
-        acc.total += transaction.amount
-      } else {
-        acc.outcome += transaction.amount
-        acc.total -= transaction.amount
-      }
-      return acc
-    },
-    { income: 0, outcome: 0, total: 0 },
-  )
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.amount
+          acc.total += transaction.amount
+        } else {
+          acc.outcome += transaction.amount
+          acc.total -= transaction.amount
+        }
+        return acc
+      },
+      { income: 0, outcome: 0, total: 0 },
+    )
+  }, [transactions])
 
   return summary
 }
